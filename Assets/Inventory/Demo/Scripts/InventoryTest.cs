78f6d27516d2f4de869d8cc482647f1d9285266f
WB_Unity <mirochacha_u_u_1106@yahoo.co.jp>
2022-11-18 01:05:04 +0900

    [Add]Inventory6

--- Assets/Inventory/Demo/Scripts/InventoryTest.cs
+++ Assets/Inventory/Demo/Scripts/InventoryTest.cs
@@ -11,17 +11,8 @@ namespace FlMr_Inventory.Demo
 
         void Start()
         {
-            // id1のアイテムを1つ追加
-            bag.AddItem(1, 1);
-            Debug.Log(bag.ToJson());
-
-            // id1のアイテムを2個削除
-            bag.RemoveItem(1, 2);
-            Debug.Log(bag.ToJson());
-
-            // id1のアイテムを1個削除
-            bag.RemoveItem(1, 1);
-            Debug.Log(bag.ToJson());
+            // id1のアイテムを2つ追加
+            bag.AddItem(1, 2);
         }
 
     }
