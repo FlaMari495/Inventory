3a756f20f25319680a22e21d39782faac1181f5e
WB_Unity <mirochacha_u_u_1106@yahoo.co.jp>
2022-11-19 16:27:08 +0900

    [Add]Inventory_v2_8

---
+++ Assets/Inventory/Demo/Scripts/Important.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace FlMr_Inventory.Demo
+{
+
+    [CreateAssetMenu(menuName = "Item/Important", fileName = "Important")]
+    public class Important : ItemBase
+    {
+
+    }
+}
